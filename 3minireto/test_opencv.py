5eab8a18856821c256850eb84c73dbae7f5ad95d
JocelynVelarde <a01285780@itesm.mx>
2026-05-13 14:32:03 -0600

    picam test

--- 3minireto/test_opencv.py
+++ 3minireto/test_opencv.py
@@ -1,6 +1,19 @@
 import cv2
-cap = cv2.VideoCapture(0)
+
+pipeline = (
+    "nvarguscamerasrc sensor-mode=4 ! "
+    "video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=30/1 ! "
+    "nvvidconv ! "
+    "video/x-raw, width=320, height=240, format=BGRx ! "
+    "videoconvert ! "
+    "video/x-raw, format=BGR ! appsink drop=1"
+)
+
+cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
 print("Abierta:", cap.isOpened())
-ok, frame = cap.read()
-print("Frame leído:", ok, "shape:", frame.shape if ok else None)
+
+for i in range(5):
+    ok, frame = cap.read()
+    print(f"Frame {i}: ok={ok}, shape={frame.shape if ok else None}")
+
 cap.release()
