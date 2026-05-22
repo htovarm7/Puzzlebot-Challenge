a0ede08ee4095ee43897ed54f0630508bf540612
Hector Tovar <h.tovarm07@gmail.com>
2026-05-21 18:19:59 -0600

    fix: correct KP and KD values for improved line follower performance

--- src/puzzlebot_challenge/puzzlebot_challenge/line_follower.py
+++ src/puzzlebot_challenge/puzzlebot_challenge/line_follower.py
@@ -7,8 +7,8 @@ WHEEL_RADIUS = 0.05154
 WHEEL_BASE   = 0.19
 FORWARD_SIGN = -1
 
-KP = 0.9
-KD = 0.1
+KP = 0.1
+KD = 0.01
 
 V_BASE    = 0.6    # m/s cruise speed
 V_MIN     = 0.04   # m/s minimum speed
