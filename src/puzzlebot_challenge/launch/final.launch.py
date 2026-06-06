b29332d87ec44c386254d8cf9894d7dcf3346ab0
Hector Tovar <h.tovarm07@gmail.com>
2026-06-05 20:15:50 -0600

    ESTA VELOCIDAD ESTA GOAT

--- src/puzzlebot_challenge/launch/final.launch.py
+++ src/puzzlebot_challenge/launch/final.launch.py
@@ -69,7 +69,7 @@ def generate_launch_description():
         DeclareLaunchArgument('kp',             default_value='0.3',  description='P gain'),
         DeclareLaunchArgument('kd',             default_value='0.08', description='D gain'),
         DeclareLaunchArgument('ka',             default_value='0.2',  description='Peso corrección ángulo'),
-        DeclareLaunchArgument('v_base',         default_value='0.2', description='Velocidad base [m/s]'),
+        DeclareLaunchArgument('v_base',         default_value='0.15', description='Velocidad base [m/s]'),
         DeclareLaunchArgument('crossing_time',  default_value='3.0',  description='Segundos en intersección recto [s]'),
         DeclareLaunchArgument('cooldown_time',  default_value='3.0',  description='Cooldown entre intersecciones [s]'),
         # Sign behaviors
